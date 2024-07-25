9d47941a2ff3560a2780aca5542bf4a12560ff66
naveen-navee <133668524+naveen-navee@users.noreply.github.com>
2024-07-25 12:42:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginRegister from "./Components/LoginRegister";
+
+function App() {
+  return (
+    <LoginRegister></LoginRegister>
+  );
+}
+
+export default App;
